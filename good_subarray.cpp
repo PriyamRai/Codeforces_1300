b4255dcc79cf90b542f14bd24ba748eb064195ee
Priyam Rai <priyamrai691@gmail.com>
2024-07-01 11:24:12 +0530

    coding it

---
+++ good_subarray.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define vi vector<int>
+using namespace std ;
+
+void input(vi &v){
+    int s=v.size() ;
+    for(int i=0;i<s;i++){
+        cin>>v[i] ;
+    }
+}
+
+
+void print(vi v){ 
+    int s=v.size() ;
+    for(int i=0;i<s;i++){
+        cout<<v[i];
+    }
+ cout<<endl ;
+}
+
+
+void solve(){
+    
+}
+
+int main()
+{
+    int t ;
+    cin>>t ;
+    while(t--){
+        solve() ;
+
+    }
+}
